2e712b9cc1c93afa2af2a74c893d847492c10f44
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-16 08:51:41 +0530

    image with text added by gridItem

--- lib/views/products.dart
+++ lib/views/products.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ProductsView extends StatelessWidget {
@@ -8,26 +10,61 @@ class ProductsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
-      padding: const EdgeInsets.all(5.0),
-      crossAxisSpacing: 15,
-      mainAxisSpacing: 15,
-      children: [...images],
+      padding: const EdgeInsets.all(15.0),
+      children: List.generate(
+        products.length,
+        (index) => GridItem(product: products[index]),
+      ),
     );
   }
 }
 
-final List<Image> images = [
-  const Image(image: AssetImage('images/heels.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels1.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels2.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels3.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels4.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels5.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels6.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels7.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels8.jpg'),fit: BoxFit.cover),
-  const Image(image: AssetImage('images/heels9.jpg'),fit: BoxFit.cover),
+class Product {
+  final String image;
+  final String title;
+  Product({required this.image, required this.title});
+}
 
+List<Product> products = [
+  Product(image: 'images/heels.jpg', title: 'High heels'),
+  Product(image: 'images/heels2.jpg', title: 'Party heels'),
+  Product(image: 'images/heels3.jpg', title: 'Stylish heels'),
+  Product(image: 'images/heels4.jpg', title: 'Red heels'),
+  Product(image: 'images/heels5.jpg', title: 'Black heels'),
+  Product(image: 'images/heels6.jpg', title: 'Trendy heels'),
+  Product(image: 'images/heels7.jpg', title: 'Fashionable heels'),
+  Product(image: 'images/heels8.jpg', title: 'White heels'),
+  Product(image: 'images/heels9.jpg', title: 'Laced heels'),
 ];
 
+class GridItem extends StatelessWidget {
+  final Product product;
+  const GridItem({super.key, required this.product});
+
+  Color getRandomColor (){
+    Random random = Random();
+    return Color.fromARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: getRandomColor(),
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.outline,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage(product.image), width: 125, height: 125, fit: BoxFit.fill),
+          Text(product.title, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),)
+        ]
+      ),
+    );
+  }
+}
 
