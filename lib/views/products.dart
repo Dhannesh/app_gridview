b43412c3a97932c905d1bfaeaa0a3809a6438b34
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-16 08:28:06 +0530

    sample images added to gridview

--- lib/views/products.dart
+++ lib/views/products.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -9,59 +8,26 @@ class ProductsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
-      padding: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(5.0),
       crossAxisSpacing: 15,
       mainAxisSpacing: 15,
-      scrollDirection: Axis.horizontal,
-      children: List.generate(products.length, (index) => GridItem(product: products[index])),
+      children: [...images],
     );
   }
 }
 
-class Product {
-  final String title;
-  final String subtitle;
-  Product({required this.title, required this.subtitle});
-}
+final List<Image> images = [
+  const Image(image: AssetImage('images/heels.jpg')),
+  const Image(image: AssetImage('images/heels1.jpg')),
+  const Image(image: AssetImage('images/heels2.jpg')),
+  const Image(image: AssetImage('images/heels3.jpg')),
+  const Image(image: AssetImage('images/heels4.jpg')),
+  const Image(image: AssetImage('images/heels5.jpg')),
+  const Image(image: AssetImage('images/heels6.jpg')),
+  const Image(image: AssetImage('images/heels7.jpg')),
+  const Image(image: AssetImage('images/heels8.jpg')),
+  const Image(image: AssetImage('images/heels9.jpg')),
 
-List<Product> products = [
-  Product(title: 'Apples', subtitle: 'Fresh Fruits'),
-  Product(title: 'Bananas', subtitle: ' Fresh Fruits'),
-  Product(title: 'Noodles', subtitle: 'Instant Fruits'),
-  Product(title: 'Ice Cream', subtitle: 'Desserts'),
-  Product(title: 'Soap', subtitle: 'Personal Care'),
-  Product(title: 'Dog Food', subtitle: 'Pet Care'),
-  Product(title: 'Shampoo', subtitle: 'Personal Care'),
-  Product(title: 'Candy', subtitle: 'Desserts'),
-  Product(title: 'Popcorn', subtitle: 'Instant Foods'),
-  Product(title: 'Fruit Juice', subtitle: 'Beverages'),
 ];
 
-class GridItem extends StatelessWidget {
-  final Product product;
-  const GridItem({super.key, required this.product});
-  Color getRandomColor() {
-    Random random = Random();
-    return Color.fromARGB(random.nextInt(255), random.nextInt(255),
-        random.nextInt(255), random.nextInt(255));
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-        color: getRandomColor(),
-        child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              Text(product.title,
-                  style: const TextStyle(
-                    fontSize: 20,
-                  )),
-              Text(product.subtitle,
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ))
-            ])));
-  }
-}
